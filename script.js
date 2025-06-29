c5a80eb47eec70e2c272f93610f1bb92c7f335f1
abuvinuya <157791523+abuvinuya@users.noreply.github.com>
2025-06-29 02:45:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ function revealLetter() {
+    const container = document.getElementById("container");
+    container.classList.add("show-paper");
+    container.classList.add("hide-envelope");
+  }
